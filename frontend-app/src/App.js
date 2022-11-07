ed320257322db859a39cf3ef6c22b4ab322dafa7
Ciro Gamboa <cirossj10@hotmail.com>
2022-11-07 00:18:30 -0500

    Added canvas element

---
+++ frontend-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CanvasDraw from "react-canvas-draw";
+
+function App() {
+  return (
+    <div className="App">
+      <CanvasDraw />
+    </div>
+  );
+}
+
+export default App;
